1f2ce7678fd761075a2e2122db767a5451b3a1d2
joaonetodevelcode <joao.neto@develcode.com.br>
2023-11-20 15:04:27 -0300

    feat: implementado funcao que retorna dados da cidade

--- src/api/apiGoogleMaps.ts
+++ src/api/apiGoogleMaps.ts
@@ -10,3 +10,12 @@ export async function getCityName(lat: number | undefined, lon: number | undefin
     
     return response.data.results[0].address_components[3].long_name
 }
+
+export async function getDataCityByName(city: string) {
+    const data: number[] = []
+    const response = await axios.get(`${url}address=${city}&key=${key}`)
+    data[0] = response.data.results[0].geometry.location.lat
+    data[1] = response.data.results[0].geometry.location.lng
+    return data
+    
+}
