226b148552d573826a8553ab229ed95b5912bb1e
joaonetodevelcode <joao.neto@develcode.com.br>
2023-11-22 10:16:27 -0300

    Adicionado novas descricoes de climas

--- src/api/climates.ts
+++ src/api/climates.ts
@@ -31,6 +31,7 @@ type ClimateType = {
   
 
 export const climates: ClimateType = {
+    //Clear
     "clear sky": {
         dinamico: clearskyDinamico,
         estatico: clearsky,
@@ -38,6 +39,7 @@ export const climates: ClimateType = {
         background: 'rgba(51, 170, 221, 1)'
     },
 
+    //clouds
     "few clouds": {
         dinamico: fewcloudsDinamico,
         estatico: fewclouds,
@@ -58,35 +60,370 @@ export const climates: ClimateType = {
         cor: ['#778899', '#708090', '#696969'],
         background: 'rgba(119, 136, 153, 1)'
     },
-    
-    "shower rain": {
-        dinamico: showerrainDinamico,
-        estatico: showerrain,
-        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
-        background: 'rgba(70, 130, 180, 0.4)'
+
+    "overcast clouds": {
+        dinamico: fewcloudsDinamico,
+        estatico: brokenclouds,
+        cor: ['#778899', '#708090', '#696969'],
+        background: 'rgba(119, 136, 153, 1)'
     },
-    
+
+    //Rain
     "rain": {
         dinamico: rainDinamico,
         estatico: rain,
         cor: ['#4682B4', '#778899', '#1E90FF'],
         background: 'rgba(70, 130, 180, 1)'
     },
+
+    "light rain": {
+        dinamico: rainDinamico,
+        estatico: rain,
+        cor: ['#4682B4', '#778899', '#1E90FF'],
+        background: 'rgba(70, 130, 180, 1)'
+    },
+
+    "moderate rain": {
+        dinamico: rainDinamico,
+        estatico: rain,
+        cor: ['#4682B4', '#778899', '#1E90FF'],
+        background: 'rgba(70, 130, 180, 1)'
+    },
+
+    "heavy intensity rain": {
+        dinamico: rainDinamico,
+        estatico: rain,
+        cor: ['#4682B4', '#778899', '#1E90FF'],
+        background: 'rgba(70, 130, 180, 1)'
+    },
+
+    "very heavy rain": {
+        dinamico: rainDinamico,
+        estatico: rain,
+        cor: ['#4682B4', '#778899', '#1E90FF'],
+        background: 'rgba(70, 130, 180, 1)'
+    },
+
+    "extreme rain": {
+        dinamico: rainDinamico,
+        estatico: rain,
+        cor: ['#4682B4', '#778899', '#1E90FF'],
+        background: 'rgba(70, 130, 180, 1)'
+    },
+
+    "freezing rain": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "light intensity shower rain": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "shower rain": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "heavy intensity shower rain": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "ragged shower rain": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
     
+    //Thunderstorm    
+    "thunderstorm with light rain": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    "thunderstorm with rain": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    "thunderstorm with heavy rain": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    "light thunderstorm": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
     "thunderstorm": {
         dinamico: thunderstormDinamico,
         estatico: thunderstorm,
         cor: ['#08244F', '#134CB5', '#0B42AB'],
         background: 'rgba(8, 36, 79, 0.6)'
     },
-    
+
+    "heavy thunderstorm": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    "ragged thunderstorm": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    "thunderstorm with light drizzle": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    "thunderstorm with drizzle": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    "thunderstorm with heavy drizzle": {
+        dinamico: thunderstormDinamico,
+        estatico: thunderstorm,
+        cor: ['#08244F', '#134CB5', '#0B42AB'],
+        background: 'rgba(8, 36, 79, 0.6)'
+    },
+
+    //Drizzle
+    "light intensity drizzle": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "drizzle": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "heavy intensity drizzle": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "light intensity drizzle rain": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "drizzle rain": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "heavy intensity drizzle rain": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "shower rain and drizzle": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "heavy shower rain and drizzle": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    "shower drizzle": {
+        dinamico: showerrainDinamico,
+        estatico: showerrain,
+        cor: ['#4682B4', '#5F9EA0', '#87CEEB'],
+        background: 'rgba(70, 130, 180, 0.4)'
+    },
+
+    //Snow    
+    "light snow": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
     "snow": {
         dinamico: snowDinamico,
         estatico: snow,
         cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
         background: 'rgba(176, 196, 222, 0.8)'
     },
+
+    "heavy snow": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "sleet": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "light shower sleet": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "shower sleet": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "snolight rain and snoww": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "rain and snow": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "light shower snow": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "shower snow": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    "heavy shower snow": {
+        dinamico: snowDinamico,
+        estatico: snow,
+        cor: ['#F5F5F5', '#B0C4DE', '#F0FFFF'],
+        background: 'rgba(176, 196, 222, 0.8)'
+    },
+
+    //Atmosphere
     "mist": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "smoke": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "sand/dust whirls": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "fog": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "sand": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "dust": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "volcanic ash": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "squalls": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "tornado": {
+        dinamico: mist,
+        estatico: mist,
+        cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
+        background: 'rgba(211, 211, 211, 0.4)'
+    },
+
+    "haze": {
         dinamico: mist,
         estatico: mist,
         cor: ['#D3D3D3', '#C0C0C0', '#A9A9A9'],
