ea9dc912fa92435d8ea2b005155d60bf1bbd711b
joaonetodevelcode <joao.neto@develcode.com.br>
2023-11-22 15:27:28 -0300

    refac/funcao que captura os dados da API

--- src/api/apiClimate.ts
+++ src/api/apiClimate.ts
@@ -1,12 +1,31 @@
 import axios from "axios";
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q="
-const key = "ba4f4c84c18821a70163d0b64c213110"
+const url = "https://api.openweathermap.org/data/2.5/"
+const key = "9f17eff0b09150069908363188a029c6"
 
 
-export async function pegarDadosClima(lat: number | undefined, lon: number | undefined) {
-    
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
+export async function getCurrentClimateData(lat: number | undefined, lon: number | undefined) {
+    const climateData: Array<any> = []
+    try{
+        const response = await axios.get(`${url}weather?lat=${lat}&lon=${lon}&appid=${key}`)
 
-    return response.data
+        const climate = response.data
+
+        if(climate) {
+            climateData[0] = climate.weather[0].description
+            climateData[1] = climate.main.temp
+            climateData[2] = climate.main.temp_max
+            climateData[3] = climate.main.temp_min
+            climateData[4] = climate.main.humidity
+            climateData[5] = climate.wind.speed
+            climateData[6] = climate.clouds.all
+        }
+
+        return climateData
+
+    } catch(erro) {
+        climateData[0] = 'Requisição falhou'
+        return climateData
+    }
 }
+
