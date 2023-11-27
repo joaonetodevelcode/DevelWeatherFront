af23fde18721956fc3ff2abb3b0a6c549eea4428
joaonetodevelcode <joao.neto@develcode.com.br>
2023-11-27 11:38:39 -0300

    refac/screenLoading e automaticLogin

--- src/screens/AutomaticLogin/index.tsx
+++ src/screens/AutomaticLogin/index.tsx
@@ -59,6 +59,6 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
     },
     content: {
-        marginBottom: '15%'
+        marginBottom: 70
     }
 })
--- src/screens/ScreenLoading/index.tsx
+++ src/screens/ScreenLoading/index.tsx
@@ -31,6 +31,6 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
     },
     content: {
-        marginBottom: '15%'
+        marginBottom: 70 
     }
 })
