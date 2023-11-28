be69579f80d39549de3afb4f3d353d0241c3eddf
joaonetodevelcode <joao.neto@develcode.com.br>
2023-11-28 17:37:10 -0300

    refac/tratando quando usuario nao da permissao

--- src/service/locationService.ts
+++ src/service/locationService.ts
@@ -1,14 +1,21 @@
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { Alert } from 'react-native';
+
+const location: number[] = []
 
 export async function requestLocation() {
     const { granted } = await requestForegroundPermissionsAsync();
-
-    if(granted) {
-        const location: number[] = []
+    
+    if(granted === true) {
         const curretPosition = await getCurrentPositionAsync();
         location[0] = curretPosition.coords.latitude;
         location[1] = curretPosition.coords.longitude;
         return location
+    }else {
+        Alert.alert("", 'O aplicativo pode não funcionar corretamente sem o acesso a sua localização', [
+            {text: 'OK'}]);
+        location[0] = -19.461045;
+        location[1] = -42.568050;
+        return location
     }
-
 }
