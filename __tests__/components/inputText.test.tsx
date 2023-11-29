430f7a66cd8c11fc23cbcacaa3e95b18027af169
joaonetodevelcode <joao.neto@develcode.com.br>
2023-11-29 12:52:12 -0300

    feat/teste para o componente InputText

---
+++ __tests__/components/inputText.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import { InputText } from '../../src/components/inputText';
+
+describe("../../src/components/inputText", () => {
+
+    test('deve chamar a função onChangeText corretamente', () => {
+        
+        const onChangeTextMock = jest.fn();
+
+        const { getByPlaceholderText } = render(
+          <InputText
+            placeholder="Type your email"
+            onChangeText={onChangeTextMock}
+            value='test@test.com'
+          />
+        );
+    
+        
+        const input = getByPlaceholderText('Type your email');
+        
+        fireEvent.changeText(input, 'test@test.com');
+       
+        
+        expect(onChangeTextMock).toHaveBeenCalledWith('test@test.com');
+        expect(input.props.value).toBe('test@test.com')
+      });
+
+})
